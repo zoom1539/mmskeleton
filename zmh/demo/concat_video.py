9051dd75f14536be092180f210e71e2abb622cfc
zhuminhang <zhumh@zhejianglab.com>
2019-11-28 09:17:20 +0800

    add demo offline

---
+++ zmh/demo/concat_video.py
@@ -0,0 +1,43 @@
+from queue import Queue
+import numpy as np
+import torch
+import torch.nn.functional as F
+import cv2
+import mmcv
+from mmcv import ProgressBar
+from mmcv.parallel import MMDataParallel
+from mmskeleton.models.backbones import ST_GCN_18
+from mmskeleton.utils import load_checkpoint
+from zmh.processor import init_pose_estimator, inference_pose_estimator
+
+video_paths = ['/media/ubuntu/share/ntu_rgbd/nturgb+d_rgb/S010C001P007R001A023_rgb.avi',
+                '/media/ubuntu/share/ntu_rgbd/nturgb+d_rgb/S010C001P007R001A024_rgb.avi',
+                '/media/ubuntu/share/ntu_rgbd/nturgb+d_rgb/S010C001P007R001A025_rgb.avi',
+                '/media/ubuntu/share/ntu_rgbd/nturgb+d_rgb/S010C001P007R001A028_rgb.avi',
+                '/media/ubuntu/share/ntu_rgbd/nturgb+d_rgb/S010C001P007R001A042_rgb.avi',
+                '/media/ubuntu/share/ntu_rgbd/nturgb+d_rgb/S010C001P007R001A043_rgb.avi',
+                '/media/ubuntu/share/ntu_rgbd/nturgb+d_rgb/S010C001P007R001A044_rgb.avi',
+                ]
+
+video_path_result = 'zmh/work_dir/test_concat.avi'
+
+def generate_video(result_frames, fps, resolution):
+
+    fourcc = cv2.VideoWriter_fourcc('M','J','P','G')  
+    videoWriter = cv2.VideoWriter(video_path_result, fourcc, fps, resolution)  
+    for frame in result_frames:
+        videoWriter.write(frame)
+    videoWriter.release()
+
+
+def main():
+    frames_total =[]
+    for path in video_paths:
+        frames = mmcv.VideoReader(path)
+        frame_list = frames[:]
+        frames_total = frames_total + frame_list
+
+    generate_video(frames_total, frames.fps, frames.resolution)
+
+if __name__ == "__main__":
+    main()
--- zmh/demo/demo_offline.py
+++ zmh/demo/demo_offline.py
@@ -1,12 +1,25 @@
 from queue import Queue
+import numpy as np
+import torch
+import torch.nn.functional as F
 import cv2
 import mmcv
+from mmcv import ProgressBar
+from mmcv.parallel import MMDataParallel
+from mmskeleton.models.backbones import ST_GCN_18
+from mmskeleton.utils import load_checkpoint
 from zmh.processor import init_pose_estimator, inference_pose_estimator
 
-video_path = 'zmh/work_dir/test.avi'
+video_path = 'zmh/work_dir/test_concat.avi'
 video_result_path = 'zmh/work_dir/test_result.avi'
 recognize_frame_num = 60
+keypoints_num = 17
+track_num = 1
+batch_size = 1
+channel_num = 3
 gpu = 0
+checkpoint = 'zmh/work_dir/stgcn.pth'
+
 detection_cfg = dict(
     model_cfg='configs/mmdet/cascade_rcnn_r50_fpn_1x.py',
     checkpoint_file= 'mmskeleton://mmdet/cascade_rcnn_r50_fpn_20e',
@@ -24,27 +37,82 @@ estimation_cfg = dict(
     )
 )
 
+model_cfg = dict(
+    in_channels = 3,
+    num_class = 3,
+    edge_importance_weighting = True,
+    graph_cfg = dict(
+        layout = "coco",
+        strategy = "spatial",
+    )
+)
+
+actions = ['kick', 'phone call', 'falling down']
+    
+
 def read_video(video_path):
     reader = mmcv.VideoReader(video_path)
     return reader
 
-def pose_estimate(pose_estimator, frame, id):
-    pose = inference_pose_estimator(pose_estimator, frame)
-    pose['frame_index'] = id
+def pre_process(pose, resolution):
+    if pose['has_return']:
+        pose['joint_preds'][:, :, :] /= resolution
+        pose['joint_preds'][:, :, :] -= 0.5
+
+        mask = (pose['joint_scores'] == 0)
+        mask = np.reshape(mask[0], pose['joint_scores'].shape[1])
+        pose['joint_preds'][:, mask, :] = 0
 
     return pose
 
-def prepare_data(frame_queue):
+def pose_estimate(pose_estimator, frame, id, resolution):
+    pose = inference_pose_estimator(pose_estimator, frame)
+    pose['frame_index'] = id
 
+    pose = pre_process(pose, resolution)
 
+    return pose
 
+def prepare_data(pose_queue):
+    data= np.zeros((batch_size, channel_num, 
+                    recognize_frame_num, keypoints_num, 
+                    track_num),
+                    dtype=np.float32)
 
+    for i in range(recognize_frame_num):
+        pose = pose_queue.get()
+        data[0, 0:2, i, :, 0] = pose['joint_preds'][0].reshape(keypoints_num,2).transpose()
+        data[0, 2, i, :, 0] = pose['joint_scores'][0].reshape(keypoints_num)
+        pose_queue.put(pose)
+
+    data = torch.from_numpy(data)
     return data
 
-def predict(data):
-    pass
+def build_model():
+    model = ST_GCN_18(**model_cfg)
 
-def render_frame(frame, pose, action_name):
+    load_checkpoint(model, checkpoint, map_location='cpu')
+    model = MMDataParallel(model, device_ids=range(1)).cuda()
+    model.eval()
+
+    return model
+    
+
+def predict(model, data):
+
+    with torch.no_grad():
+        output = model(data).data
+        output = output[0]
+        score = F.softmax(output, dim=0).cpu().numpy()
+
+    max_id = np.argmax(score)
+
+    action_name = ''
+    if score[max_id] > 0.7:
+        action_name = actions[max_id]
+    return action_name, score[max_id]
+
+def render_frame(frame, pose, action_name, score):
     cv2.putText(frame, 
                 action_name, 
                 (100,100), 
@@ -53,12 +121,20 @@ def render_frame(frame, pose, action_name):
                 (255,0,0), 
                 5, 
                 cv2.LINE_AA)
+    cv2.putText(frame, 
+                str(score), 
+                (100,200), 
+                cv2.FONT_HERSHEY_SIMPLEX, 
+                2,
+                (255,0,0), 
+                5, 
+                cv2.LINE_AA)
     return frame
 
-def generate_video(result_frames):
+def generate_video(result_frames, resolution):
     fps = 30   
     fourcc = cv2.VideoWriter_fourcc('M','J','P','G')  
-    videoWriter = cv2.VideoWriter(video_result_path, fourcc, fps, (1920,1080))  
+    videoWriter = cv2.VideoWriter(video_result_path, fourcc, fps, resolution)  
     for frame in result_frames:
         videoWriter.write(frame)
     videoWriter.release()
@@ -67,28 +143,34 @@ def generate_video(result_frames):
 def main():
     frames = read_video(video_path)
 
-    frame_queue = Queue(recognize_frame_num)
+    pose_queue = Queue(recognize_frame_num)
     result_frames = []
 
     pose_estimator = init_pose_estimator( detection_cfg, estimation_cfg, device = gpu)
+    model = build_model()
+
+    prog_bar = ProgressBar(len(frames))
 
     for i, frame in enumerate(frames):
-        pose = pose_estimate(pose_estimator, frame, i)
+        prog_bar.update()
 
-        if frame_queue.qsize() < recognize_frame_num:
-            
-            frame_queue.put(pose)
+        pose = pose_estimate(pose_estimator, frame, i, frames.resolution)
+
+        if pose_queue.qsize() < recognize_frame_num:
+
+            pose_queue.put(pose)
             result_frames.append(frame)
             continue
         else:
-            frame_queue.get()
-            frame_queue.put(pose)
-            data = prepare_data(frame_queue)
-            action_name = predict(data)
-            frame = render_frame(frame, pose, action_name)
+            pose_queue.get()
+            pose_queue.put(pose)
+            data = prepare_data(pose_queue)
+            action_name, score = predict(model, data)
+            frame = render_frame(frame, pose, action_name, score)
             result_frames.append(frame)
+        
     
-    generate_video(result_frames)
+    generate_video(result_frames, frames.resolution)
 
 if __name__ == "__main__":
     main()
